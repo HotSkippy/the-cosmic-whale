492832b8a299ad643ab83e01ec28e84151a9b73e
Roldan Melcon <roldan@thecosmicwhale.com>
2016-11-23 02:55:34 +0000

    initial deploy

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+app.set("view engine", "ejs");
+
+// ROOT ROUTE
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("THE COSMIC WHALE HAS SHIPPED");
+})
